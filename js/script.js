5fc135092eb7d1af02c079e11e09846dd566dd09
Bram <bramstrik@live.com>
2024-03-29 14:52:08 +0100

    Fix checkWin function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const GameBoard = (function () {
     const SIZE = 3;
-    const board = [];
-    for (let row = 0; row < SIZE; row++) {
-        board.push(new Array(SIZE));
+    let board = [];
+    const reset = function () {
+        board = [];
+        for (let row = 0; row < SIZE; row++) {
+            board.push(new Array(SIZE));
+        }
     }
     const putAt = function (row, col, symbol) {
         if (board[row][col] == undefined) {
@@ -19,27 +22,37 @@ const GameBoard = (function () {
         // If not, check to see who won (if anyone)
         let eval = false;
         for (let i = 0; i < SIZE; i++) {
-            if (board[i][0] == board[i][1] == board[i][2] != undefined) {
+            if (board[i][0] == board[i][1] &&
+                board[i][0] == board[i][2] &&
+                board[i][0] != undefined) {
                 eval = board[i][0];
             };
         }
         for (let i = 0; i < SIZE; i++) {
-            if (board[0][i] == board[1][i] == board[2][i] != undefined) {
+            if (board[0][i] == board[1][i] &&
+                board[0][i] == board[2][i] &&
+                board[0][i] != undefined) {
                 eval = board[0][i];
             };
         }
-        if (board[0][0] == board[1][1] == board[2][2] != undefined ||
-            board[0][2] == board[1][1] == board[2][0] != undefined) {
+        if ((board[0][0] == board[1][1] &&
+             board[0][0] == board[2][2] &&
+             board[0][0] != undefined) ||
+            (board[0][2] == board[1][1] && 
+             board[0][2] == board[2][0] &&
+             board[0][2] != undefined)) {
             eval = board[0][0];
         };
 
-        if (!eval && board.flat().length != SIZE ** 2) {
+        if (!eval && board.flat().length == SIZE ** 2) {
+            console.log(board.flat().length);
             eval = 'draw';
         }
 
         return eval;
     }
-    return { getBoard: getBoard, putAt: putAt, checkWin: checkWin };
+    reset();
+    return { getBoard: getBoard, putAt: putAt, checkWin: checkWin, reset: reset };
 });
 const Game = (function (gameBoard, player1, player2) {
     const playerOne = player1;
@@ -54,6 +67,7 @@ const Game = (function (gameBoard, player1, player2) {
         let hasWon = board.checkWin();
         if (hasWon) {
             console.log(hasWon);
+            board.reset();
         }
 
         printGameInfo();
